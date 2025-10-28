e6026722a47122da4011c1ab7365dc2fdf12a905
utkarshadahibhate <utkarsha.dahibhate@gmail.com>
2025-10-28 13:48:24 +0000

    28/10/25

---
+++ 1.Practice_V1/enum_typdef.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+enum day {mon = 1, tue = 7, wed, thu = 10, fri, sat, sun}; // set of constants under day
+
+typedef int marks;
+
+int main(){
+    // day d;
+    // d = mon;
+    // d = tue;
+    // cout<<d<<endl;
+
+    // cout<<mon<<endl;
+    // cout<<tue<<endl;
+    // cout<<wed<<endl;
+    // cout<<thu<<endl;
+    // cout<<fri<<endl;
+    // cout<<sat<<endl;
+    // cout<<sun<<endl;
+
+    //m1 and m2 are for storing marks
+    marks m1, m2;
+    m1 = 80;
+    m2 = 60;
+
+    cout<<15%2+3<<endl;
+
+}
